81a63d5a649021563e030635a13fbe0cc1169365
IvanHoracio1901 <ivan53373@gmail.com>
2020-05-24 10:55:28 -0500

    Agregar archivos de proyecto.

---
+++ TestMadeAstar/Controllers/TenantPersonnelController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMadeAstar.Models;
+
+namespace TestMadeAstar.Controllers
+{
+    public class TenantPersonnelController : Controller
+    {
+        // GET: TenantPersonnel
+        public ActionResult Index()
+        {
+            try
+            {
+                using (var db = new TestDemoAstarEntities())
+                {
+                    return View(db.TenantPersonnel.ToList());
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public ActionResult AddT()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddT(TenantPersonnel person)
+        {
+           
+                try
+            {
+                using (var db = new TestDemoAstarEntities())
+                {
+                    person.DOB = DateTime.Now;
+                    
+                        db.TenantPersonnel.Add(person);
+                        if (ModelState.IsValid)
+                        {
+                            db.SaveChanges();
+                           
+                        }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("Error to add", ex);
+                return View();
+            }
+        }
+        public ActionResult EditT(int id)
+        {
+            try
+            {
+                using (var db = new TestDemoAstarEntities())
+                {
+                    TenantPersonnel person = db.TenantPersonnel.Where(a => a.TenantId == id).FirstOrDefault();
+                    return View(person);
+                }
+            }
+            catch (Exception ex)
+            {
+;
+                throw;
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditT(TenantPersonnel person)
+        {
+            try
+            {
+                using (var db = new TestDemoAstarEntities())
+                {
+                    TenantPersonnel user = db.TenantPersonnel.Find(person.TenantId);
+                    user.FirstName = person.FirstName;
+                    user.MiddleName = person.MiddleName;
+                    user.LastName = person.LastName;
+                    user.GenderFk = person.GenderFk;
+                    user.NickName = person.NickName;
+                    user.PrefixID = person.PrefixID;
+                    if (ModelState.IsValid)
+                    {
+                        db.SaveChanges();
+
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("Error to edit", ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                using (var db = new TestDemoAstarEntities())
+                {
+                    TenantPersonnel person = db.TenantPersonnel.Find(id);
+                    return View(person);
+                }
+            }
+            catch (Exception ex)
+            {
+                ;
+                throw;
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new TestDemoAstarEntities())
+                {
+                    TenantPersonnel person = db.TenantPersonnel.Find(id);
+                    db.TenantPersonnel.Remove(person);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+
+    }
+}
---
+++ TestMadeAstar/Models/TennatPersonnelBD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TestMadeAstar.Models
+{
+    public class TennatPersonnelBD
+    {
+        [Required]
+        public int TenantId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Give me your name Name please")]
+        public virtual string FirstName { get; set; }
+        [Required]
+        [StringLength (50, MinimumLength = 4, ErrorMessage = "Give me your name middleName please")]
+        public string MiddleName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Give me your name LastName please")]
+        public virtual string LastName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "Give me your name NickName please")]
+        public virtual string NickName { get; set; }
+        public virtual DateTime DOB { get; set; }
+        public virtual string Active { get; set; }
+        [ForeignKey("GenderId")]
+        public Gender GenderFk { get; set; }
+        
+        public int PrefixID { get; set; }
+        [Required]
+        public virtual Gender Gender { get; set; }
+    }
+}
